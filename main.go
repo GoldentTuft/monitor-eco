f360591bac6c0dca2ea00498bcf43e285833c817
GoldentTuft <goldent@outlook.jp>
2020-07-09 21:47:14 +0900

    first

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"syscall"
+	"time"
+
+	"golang.org/x/sys/windows"
+)
+
+type (
+	HANDLE uintptr
+	HWND   HANDLE
+)
+
+const (
+	HWND_BROADCAST  = HWND(0xFFFF)
+	WM_SYSCOMMAND   = 274
+	SC_MONITORPOWER = 0xF170
+	// DISPLAY_ON      = ^uint(0)
+	// DISPLAY_ON      = -1 どっちもだめ。やり方が分からず。
+	DISPLAY_OFF = 2
+)
+
+var (
+	user32          = windows.NewLazySystemDLL("user32.dll")
+	postMessageProc *windows.LazyProc
+)
+
+func init() {
+	postMessageProc = user32.NewProc("PostMessageA")
+}
+
+func monitorSwitch(ms uint) {
+	res, _, _ := syscall.Syscall6(postMessageProc.Addr(), 4,
+		uintptr(HWND_BROADCAST),
+		uintptr(WM_SYSCOMMAND),
+		uintptr(SC_MONITORPOWER),
+		uintptr(ms),
+		0, 0)
+	fmt.Println(res)
+}
+func main() {
+	for {
+		monitorSwitch(DISPLAY_OFF)
+		time.Sleep(60 * time.Second)
+	}
+
+	// これもだめ。
+	// x := -1
+	// fmt.Println("piyo")
+	// monitorSwitch(uint(x))
+}
