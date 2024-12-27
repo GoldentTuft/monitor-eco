2a0ac4c22b5c4f1d1168c08b6d5a1bd4959812b2
GoldentTuft <goldent@outlook.jp>
2024-12-28 03:11:27 +0900

    カーソル移動で自動終了など

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"syscall"
+	"math"
 	"time"
+	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
@@ -23,16 +24,13 @@ const (
 )
 
 var (
-	user32          = windows.NewLazySystemDLL("user32.dll")
-	postMessageProc *windows.LazyProc
+	user32           = windows.NewLazySystemDLL("user32.dll")
+	postMessageProc  = user32.NewProc("PostMessageA")
+	procGetCursorPos = user32.NewProc("GetCursorPos")
 )
 
-func init() {
-	postMessageProc = user32.NewProc("PostMessageA")
-}
-
 func monitorSwitch(ms uint) {
-	syscall.Syscall6(postMessageProc.Addr(), 4,
+	postMessageProc.Call(
 		uintptr(HWND_BROADCAST),
 		uintptr(WM_SYSCOMMAND),
 		uintptr(SC_MONITORPOWER),
@@ -40,24 +38,68 @@ func monitorSwitch(ms uint) {
 		0, 0)
 }
 
+type Pos struct {
+	X, Y int32
+}
+
+func getCursorPos() (Pos, error) {
+	var pos Pos
+	ret, _, err := procGetCursorPos.Call(uintptr(unsafe.Pointer(&pos)))
+	if ret == 0 {
+		return pos, err
+	}
+	return pos, nil
+}
+
+func clearLine() {
+	fmt.Print("\r                    \r")
+}
+
 func main() {
-	const start = 5
-	const cycle = 60 * time.Second
-	for i := 0; i < start; i++ {
-		fmt.Printf("\r消灯まで%d秒前", start-i)
+	// カウントダウン
+	const countdownStart = 5
+	for i := 0; i < countdownStart; i++ {
+		clearLine()
+		fmt.Printf("消灯まで%d秒前", countdownStart-i)
 		time.Sleep(1 * time.Second)
 	}
-	fmt.Printf("\r\n")
-	var duration time.Duration
-	for {
-		monitorSwitch(DISPLAY_OFF)
-		time.Sleep(cycle)
-		duration += cycle
-		fmt.Printf("\r%v経過", duration)
-	}
 
-	// これもだめ。
-	// x := -1
-	// fmt.Println("piyo")
-	// monitorSwitch(uint(x))
+	// 指定間隔でモニターを消す
+	go func() {
+		const cycle = 60 * time.Second
+		var duration time.Duration
+		for {
+			monitorSwitch(DISPLAY_OFF)
+			var cycleCount time.Duration
+			for cycleCount = 0; cycleCount < cycle; {
+				clearLine()
+				fmt.Printf("\r%v経過", duration)
+				time.Sleep(1 * time.Second)
+				cycleCount += 1 * time.Second
+				duration += 1 * time.Second
+			}
+		}
+	}()
+
+	// カーソル指定距離以上移動したら終了
+	exitDistance := 400.0
+	startPos, err := getCursorPos()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	for {
+		pos, err := getCursorPos()
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		xd := pos.X - startPos.X
+		yd := pos.Y - startPos.Y
+		d := math.Sqrt(math.Pow(float64(xd), 2) + math.Pow(float64(yd), 2))
+		if d >= exitDistance {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
